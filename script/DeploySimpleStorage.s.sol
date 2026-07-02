1e64b5a94c189b91765966fbe5b760c28220abd3
LewisJassy <jassylewis70@gmail.com>
2026-07-02 12:45:10 +0300

    update the transfer func to use call , which is more safer since instead of reverting the money it will return false

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.35;
 
 import {Script} from "forge-std/Script.sol";
-import {SimpleStorage} from "../contracts/SimpleStorage.sol";
+import {SimpleStorage, Bank} from "../contracts/SimpleStorage.sol";
 
 contract DeploySimpleStorgae is Script {
     function run() external {
         vm.startBroadcast();
 
         new SimpleStorage();
+        new Bank();
         vm.stopBroadcast();
     }
 }
